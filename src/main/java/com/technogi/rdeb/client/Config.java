5c3431874d18d0298f951a4ba8b4eb8ae87b2e77
Carlos Hernandez <carlos@technogi.com.mx>
2017-08-21 13:13:31 -0500

    test1

--- src/main/java/com/technogi/rdeb/client/Config.java
+++ src/main/java/com/technogi/rdeb/client/Config.java
@@ -1,4 +1,24 @@
 package com.technogi.rdeb.client;
 
 public class Config {
+  private int pollingTime = 5;
+  private String connectionUrl;
+
+  public int getPollingTime() {
+    return pollingTime;
+  }
+
+  public Config setPollingTime(int pollingTime) {
+    this.pollingTime = pollingTime;
+    return this;
+  }
+
+  public String getConnectionUrl() {
+    return connectionUrl;
+  }
+
+  public Config setConnectionUrl(String connectionUrl) {
+    this.connectionUrl = connectionUrl;
+    return this;
+  }
 }
